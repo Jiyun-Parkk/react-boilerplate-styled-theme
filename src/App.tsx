ca7d126afde44a6565604397d4dbdb4cc53c7a80
박지윤 <jyun1937@naver.com>
2023-01-09 13:28:54 +0900

    Fix: template 구조 해제

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Router from "Router";
+import { GlobalStyle } from "style/global.style";
+import { dark, light } from "style/theme";
+import { ThemeProvider } from "styled-components";
+import { useAppSelector } from "hooks/useStore";
+
+export const App = () => {
+  const isDark = useAppSelector(({ theme }) => theme);
+
+  return (
+    <ThemeProvider theme={isDark ? dark : light}>
+      <Router />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
+
+export default App;
